14f56ea1bdbc1d0c26a0370d9044ab2be1bef26f
Elliott Choi <elliottchoi09alt@gmail.com>
2026-06-28 15:06:25 -0500

    Update email data

---
+++ data.js
@@ -0,0 +1,23 @@
+var MRO_DATA = [
+  {
+    "customer": "Delta TechOps",
+    "rfq": "123456",
+    "ro": "RO-010101",
+    "pn": "CFM56-7B",
+    "sn": "987654321",
+    "stage": "RFQ/capability request",
+    "summary": "Customer requests repair capability check and quote for CFM56-7B engine part.",
+    "date": "Sun, 28 Jun 2026 15:06:03 -0500",
+    "from": "Elliott Choi <elliottchoi09alt@gmail.com>",
+    "body": "Dear Example MRO,\r\n\r\nPlease check the repair capabilities for this part and provide a quote.\r\n\r\nPart Number: CFM56-7B\r\n\r\nSerial Number: 987654321\r\n\r\nRepair Order: RO-010101\r\n\r\nRFQ Number: 123456\r\n\r\nBest regards,\r\n\r\nDelta TechOps\r\n",
+    "messages": [
+      {
+        "dir": "inbound",
+        "from": "Elliott Choi <elliottchoi09alt@gmail.com>",
+        "time": "15:06",
+        "body": "Dear Example MRO,\r\n\r\nPlease check the repair capabilities for this part and provide a quote.\r\n\r\nPart Number: CFM56-7B\r\n\r\nSerial Number: 987654321\r\n\r\nRepair Order: RO-010101\r\n\r\nRFQ Number: 123456\r\n\r\nBest regards,\r\n\r\nDelta TechOps\r\n"
+      }
+    ],
+    "docs": []
+  }
+];
